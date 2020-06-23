d21760182e335dcfc048af08d2032a113623f41d
Aaron Timbol <timbolaj@gmail.com>
2020-06-23 21:27:25 +0000

    formatted all prices and price totals

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,6 +2,9 @@ class Admin::ProductsController < ApplicationController
 
   def index
     @products = Product.order(id: :desc).all
+    @prices = []
+    @products.each  {|item| @prices.push(item.price) }
+
   end
 
   def new
