69b71dd15e31d539e4d128e79b60212492b2fc71
Aaron Timbol <timbolaj@gmail.com>
2020-06-25 19:24:17 +0000

    Finished styling, and added the fields first and last name from the name field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id 
+      session[:user_id] = @user.id 
       redirect_to '/'
     else
       render :new
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
