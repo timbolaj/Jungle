74834dccaf720a601e72bd88a49eb49305c4913d
Aaron Timbol <timbolaj@gmail.com>
2020-06-25 18:59:27 +0000

    login page styled and error messages created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :exception
-
-  private
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -14,6 +11,8 @@ class ApplicationController < ActionController::Base
   def authorize
     redirect_to '/login' unless current_user
   end
+
+  private
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
+  validates :password_confirmation, presence: true
 end
