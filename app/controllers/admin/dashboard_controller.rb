485729496d3cd4db2da34b1f9009d928f54f56e5
Aaron Timbol <timbolaj@gmail.com>
2020-06-25 13:32:26 +0000

    modified the dashboard to reflect updates in Compass

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,18 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
-    @category_name_to_id = {}
-    @product_per_category_count = {}
+    @category = Category.all
     @product = Product.all
-    @product_category = Category.joins(:products)
-
-    @product_category.map do |n|
-      unless @product_per_category_count[n.name] == nil
-        @product_per_category_count[n.name] += 1
-      else
-        @product_per_category_count[n.name] = 1
-        @category_name_to_id[n.name] = n.id
-      end
-    end
   end
 end
