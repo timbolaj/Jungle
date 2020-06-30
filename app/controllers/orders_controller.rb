a6a4c9a82db6e4f6d6c02ae56fb8e1409d33ee48
Aaron Timbol <timbolaj@gmail.com>
2020-06-30 23:28:46 +0000

    preview page renders and functions are all working; missing line items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,7 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
+      OrderMailer.send(order)      
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
     else
